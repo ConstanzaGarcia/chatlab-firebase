b3411c1b094f1052a54023a617466dcd9c0916fa
ConstanzaGarcia <o.conii.o@gmail.com>
2018-07-10 10:03:40 -0400

    intentando registrar, error linea 2

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+window.onload = () => {
+    firebase.auth().onAuthStateChanged((user)=>{
+        if (user) {
+            //si estamos logueados
+            loggedOut.style.display = "none";
+            loggedIn.style.display = "blick";
+            console.log("User > " + JSON.stringify(user));
+        } else {
+            //no estamos logueados
+            loggedOut.style.display = "block";
+            loggedIn.style.display = "none";
+        }
+
+    });
+}
+
+
 function logInOrRegister() {
     const emailValue = email.value;
     const passwordValue = password.value;
@@ -7,7 +24,20 @@ function logInOrRegister() {
             console.log("Usuario registrado");
         })
         .catch((error) => {
-            console.log("Error de firebase >" +error.code);
-            console.log("Error de firebase, mensaje >" +error.message);
+            console.log("Error de firebase >" + error.code);
+            console.log("Error de firebase, mensaje >" + error.message);
         })
+}
+
+function login() {
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    firebase.auth().signInWhithEmailAndPassword(emailValue, passwordValue)
+        .then(() => {
+            console.log("Usuario con login exitoso");
+        })
+        .catch((error) => {
+            console.log("Error de firebase >" + error.code);
+            console.log("Error de firebase, mensaje >" + error.message);
+        });
 }
